ae7eceafaf850ad731263b511364eb41ea2ca9d4
Angus Fisk <angus.fisk@ndcn.ox.ac.uk>
2018-10-30 11:13:50 +0200

    Updates with working short and long actogram scripts
    Creates both png and svg versions of all actograms, can be rerun
    whenever!!!

--- 02_createactograms/02_shortactogram.py
+++ 02_createactograms/02_shortactogram.py
@@ -11,14 +11,18 @@ input_directory = pathlib.Path("/Users/angusfisk/Documents/01_PhD_files/"
                                 "01_projects/P2_Circ_Disruption_paper_chapt2/"
                                 "03_data_files")
 save_directory = input_directory / "../02_analysis_outputs"
-subdir_name = '01_long_actograms'
+subdir_name = '02_short_actograms'
 
 fig_object = prep.SaveObjectPipeline(input_directory=input_directory,
                                      save_directory=save_directory)
 
+fig_object.process_file(module=prep,
+                        function_name="slice_by_label_col",
+                        subdir_name=subdir_name)
+
 fig_object.create_plot(function_name="actogram_plot_all_cols",
                        module=act,
-                       data_list=fig_object.df_list,
+                       data_list=None,
                        subdir_name=subdir_name,
                        savefig=True,
                        save_suffix=".png",
@@ -26,9 +30,8 @@ fig_object.create_plot(function_name="actogram_plot_all_cols",
 
 fig_object.create_plot(function_name="actogram_plot_all_cols",
                        module=act,
-                       data_list=fig_object.df_list,
+                       data_list=None,
                        subdir_name=subdir_name,
                        savefig=True,
                        save_suffix=".svg",
                        figsize=(10,10))
-
