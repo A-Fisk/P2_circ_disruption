2acac8c81f1a19c2fce4d3f2ed202c8865e1ea75
Angus Fisk <angus.fisk@ndcn.ox.ac.uk>
2018-11-02 18:27:50 +0200

    Updates sleep processing script to only look for sleep where major activity is taking place
    Defines major activity as 30minute mean >9.
    Problem with getting LDR to stick in sleep dfs <-- too confused and
    tired now to think straight about it, come back

--- 04_sleep/01_episodes/01_create_files.py
+++ 04_sleep/01_episodes/01_create_files.py
@@ -6,18 +6,16 @@ import sys
 sys.path.insert(0, "/Users/angusfisk/Documents/01_PhD_files/07_python_package/actigraphy_analysis")
 import actigraphy_analysis.preprocessing as prep
 import actigraphy_analysis.episodes as ep
-import actigraphy_analysis.sleep_process as sleep
 
 input_directory = pathlib.Path("/Users/angusfisk/Documents/01_PhD_files/"
                                 "01_projects/P2_Circ_Disruption_paper_chapt2/"
-                                "03_data_files")
+                                "03_data_files/02_sleep")
 save_directory = input_directory
-subdir_name = "02_sleep"
+subdir_name = "01_episodes"
 
-sleep_object = prep.SaveObjectPipeline(input_directory=input_directory,
-                                       save_directory=save_directory)
-sleep_object.process_file(module=sleep,
-                          function_name="create_scored_df",
-                          subdir_name=subdir_name,
-                          savecsv=True)
-
+episode_object = prep.SaveObjectPipeline(input_directory=input_directory,
+                                         save_directory=save_directory)
+episode_object.process_file(module=ep,
+                            function_name="create_episode_df",
+                            subdir_name=subdir_name,
+                            savecsv=True)
