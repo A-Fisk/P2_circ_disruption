d4e396f99d8fc9f6dcdfbed809199c68c4cedcb5
Angus Fisk <angus.fisk@ndcn.ox.ac.uk>
2018-12-10 18:24:36 +0000

    works to get sum histogram

---
+++ 02_activity/02_episodes/04_histogram_sum.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import pathlib
+import sys
+sys.path.insert(0, "/Users/angusfisk/Documents/01_PhD_files/"
+                    "07_python_package/actiPy")
+import actiPy.preprocessing as prep
+import actiPy.episodes as ep
+
+# define the input directoryies
+activity_dir = pathlib.Path("/Users/angusfisk/Documents/01_PhD_files/"
+                              "01_projects/P2_Circ_Disruption_paper_chapt2/"
+                              "01_data_files/01_activity/01_episodes")
+sleep_dir = activity_dir.parents[1] / "02_sleep/01_episodes"
+input_list = [activity_dir, sleep_dir]
+
+# define the save directories
+activity_save = activity_dir.parents[2] / '03_analysis_outputs/01_activity'
+sleep_save = activity_save.parents[0] / '02_sleep'
+output_list = [activity_save, sleep_save]
+
+# define the subdirectory in the save directory to create and save in
+subdir_name = '03_episodes/01_sum'
+
+# define the keywords for reading in the file
+init_kwargs = {
+    "subdir_name": subdir_name,
+    "func": (prep, "read_file_to_df"),
+    "index_col": [0, 1],
+    "header": [0]
+}
+
+# define process kwargs
+process_kwargs = {
+    "function": (ep, "_stack_all_values")
+}
+
+
+# define the keywords to plot the file
+plot_kwargs = {
+    "function": (ep, "episode_histogram"),
+    "data_list": "processed",
+    "logy": True,
+    "remove_col": False,
+    "showfig": False,
+    "savefig": True,
+    "xlabel": "Episode Duration (seconds)",
+    "figsize": (20, 10),
+}
+
+
+# apply process to both activity and sleep
+for input, save in zip(input_list, output_list):
+
+    # copy the kwargs
+    curr_init = init_kwargs
+    curr_process = process_kwargs
+    curr_plot = plot_kwargs
+
+    # modify the init and plot kwargs if necessary
+    curr_init["input_directory"] = input
+    curr_init["save_directory"] = save
+
+    # process all the files
+    ep_object = prep.SaveObjectPipeline(**curr_init)
+    ep_object.process_file(**curr_process)
+    ep_object.create_plot(**curr_plot)
