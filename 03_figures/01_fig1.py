752e60afd72f005ad6909648757856e55a0490f3
Angus Fisk <angus.fisk@ndcn.ox.ac.uk>
2019-04-04 10:28:50 +0100

    Tidies x and y label position

--- 03_figures/01_fig1.py
+++ 03_figures/01_fig1.py
@@ -1,5 +1,7 @@
 # Figure 1 - representative actograms of each condition
 
+import actiPy.actogram_plot as aplot
+import actiPy.preprocessing as prep
 import pathlib
 import pandas as pd
 import numpy as np
@@ -9,9 +11,7 @@ import matplotlib.gridspec as gs
 import matplotlib.dates as mdates
 import sys
 sys.path.insert(0, "/Users/angusfisk/Documents/01_PhD_files/"
-                    "07_python_package/actiPy")
-import actiPy.preprocessing as prep
-import actiPy.actogram_plot as aplot
+                "07_python_package/actiPy")
 
 # define constants
 index_cols = [0, 1]
@@ -20,7 +20,7 @@ save_fig = pathlib.Path("/Users/angusfisk/Documents/01_PhD_files/"
                         "01_projects/01_thesisdata/02_circdis/"
                         "03_analysis_outputs/03_figures/01_fig1.png")
 
-# import the files we are going to read ########################################
+# import the files we are going to read ##################################
 activity_dir = pathlib.Path('/Users/angusfisk/Documents/01_PhD_files/'
                             '01_projects/01_thesisdata/02_circdis/'
                             '01_data_files/01_activity/00_clean')
@@ -43,7 +43,7 @@ sleep_dfs = [prep.read_file_to_df(x, index_col=index_cols)
 activity_df = pd.concat(dict(zip(activity_files, activity_dfs)))
 sleep_df = pd.concat(dict(zip(activity_files, sleep_dfs)))
 
-##### Plotting - ###############################################################
+##### Plotting - #########################################################
 
 # plotting constants
 conditions = activity_df.index.get_level_values(0).unique()
@@ -73,8 +73,8 @@ actogram_kwargs = {
     "set_file_title": False,
     "linewidth": 0.1,
     "day_label_size": 6,
-    "ylabelpos": (0.1, 0.5),
-    "xlabelpos": (0.5, 0.1),
+    "ylabelpos": (0.05, 0.5),
+    "xlabelpos": (0.5, 0.13),
     "title": "Double plotted actograms of activity and sleep"
 }
 timelabelsize = 5
@@ -96,15 +96,15 @@ activity_axes = [plt.subplot(x) for x in activity_grid]
 for condition, axis in zip(conditions, activity_axes):
     data = activity_df.loc[condition]
     animal = anim_dict[condition]
-    
+
     # Plot on a clean axis
     axis.set(yticks=[],
              xticks=[])
-    axis.text(-0.2, # panel text
+    axis.text(-0.2,  # panel text
               1.1,
               ac_panels[condition],
               transform=axis.transAxes)
-    axis.text(-0.3, # condition text
+    axis.text(-0.3,  # condition text
               0.5,
               condition,
               transform=axis.transAxes,
@@ -115,10 +115,10 @@ for condition, axis in zip(conditions, activity_axes):
                                               subplot=axis,
                                               timeaxis=True,
                                               **actogram_kwargs)
-    
+
     acax.tick_params(axis="x", which='major', labelsize=timelabelsize)
-    
-    
+
+
 # Plot sleep actograms on the right column
 sleep_grid = gs.GridSpec(nrows=len(conditions),
                          ncols=1,
@@ -132,26 +132,26 @@ sleep_axes = [plt.subplot(x) for x in sleep_grid]
 for condition, axis_1 in zip(conditions, sleep_axes):
     sleep_data = sleep_df.loc[condition]
     animal = anim_dict[condition]
-    
+
     # plot on clean axis
     axis_1.set(yticks=[],
                xticks=[])
-    axis_1.text(-0.2, # panel text
+    axis_1.text(-0.2,  # panel text
                 1.1,
                 sl_panels[condition],
                 transform=axis_1.transAxes)
-    sfig, sax  = aplot._actogram_plot_from_df(sleep_data,
-                                              animal,
-                                              fig=fig,
-                                              subplot=axis_1,
-                                              timeaxis=True,
-                                              ylim=sleep_ylim,
-                                              **actogram_kwargs)
-    
+    sfig, sax = aplot._actogram_plot_from_df(sleep_data,
+                                             animal,
+                                             fig=fig,
+                                             subplot=axis_1,
+                                             timeaxis=True,
+                                             ylim=sleep_ylim,
+                                             **actogram_kwargs)
+
     # tidy timelabels
     sax.tick_params(axis="x", which='major', labelsize=timelabelsize)
-    
-    
+
+
 # Add in further labels
 type_label_level = 1.1
 type_label_xval = 0.35
