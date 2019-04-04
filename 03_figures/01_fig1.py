047581ec65f0e888f81266b9c3973636e5350940
Angus Fisk <angus.fisk@ndcn.ox.ac.uk>
2019-04-04 10:15:20 +0100

    Fixed line edge colour
    Was drawing a white background around each line - couldn't
    see LD cycles because of it.
    Removed with setting patch.force_edgecolor to False

--- 03_figures/01_fig1.py
+++ 03_figures/01_fig1.py
@@ -1,4 +1,5 @@
-# start with imports
+# Figure 1 - representative actograms of each condition
+
 import pathlib
 import pandas as pd
 import numpy as np
@@ -11,13 +12,6 @@ sys.path.insert(0, "/Users/angusfisk/Documents/01_PhD_files/"
                     "07_python_package/actiPy")
 import actiPy.preprocessing as prep
 import actiPy.actogram_plot as aplot
-import actiPy.periodogram as per
-import actiPy.waveform as wave
-
-# Figure 1
-# actograms as subplots
-
-# import the files we are going to read
 
 # define constants
 index_cols = [0, 1]
@@ -26,7 +20,7 @@ save_fig = pathlib.Path("/Users/angusfisk/Documents/01_PhD_files/"
                         "01_projects/01_thesisdata/02_circdis/"
                         "03_analysis_outputs/03_figures/01_fig1.png")
 
-# get the file names
+# import the files we are going to read ########################################
 activity_dir = pathlib.Path('/Users/angusfisk/Documents/01_PhD_files/'
                             '01_projects/01_thesisdata/02_circdis/'
                             '01_data_files/01_activity/00_clean')
@@ -49,7 +43,7 @@ sleep_dfs = [prep.read_file_to_df(x, index_col=index_cols)
 activity_df = pd.concat(dict(zip(activity_files, activity_dfs)))
 sleep_df = pd.concat(dict(zip(activity_files, sleep_dfs)))
 
-##### Plotting - each group
+##### Plotting - ###############################################################
 
 # plotting constants
 conditions = activity_df.index.get_level_values(0).unique()
@@ -61,73 +55,121 @@ anim_dict = {
     conditions[2]: 3,
     conditions[3]: 3
 }
+ac_panels = {
+    conditions[0]: "A",
+    conditions[1]: "B",
+    conditions[2]: "C",
+    conditions[3]: "D"
+}
+sl_panels = {
+    conditions[0]: "E",
+    conditions[1]: "F",
+    conditions[2]: "G",
+    conditions[3]: "H"
+
+}
 actogram_kwargs = {
     "drop_level": True,
     "set_file_title": False,
     "linewidth": 0.1,
+    "day_label_size": 6,
+    "ylabelpos": (0.1, 0.5),
+    "xlabelpos": (0.5, 0.1),
+    "title": "Double plotted actograms of activity and sleep"
 }
-timelabelsize=6
+timelabelsize = 5
 
 # initialise figure
 fig = plt.figure()
+plt.rcParams['patch.force_edgecolor'] = False
 
-# create the left column for activity
-activity_grid = gs.GridSpec(nrows=len(conditions), ncols=1, figure=fig,
-                            right=0.4, hspace=hspace)
+# Plot activity actograms on left column
+activity_grid = gs.GridSpec(nrows=len(conditions),
+                            ncols=1,
+                            figure=fig,
+                            right=0.45,
+                            left=0.15,
+                            hspace=hspace)
 activity_axes = [plt.subplot(x) for x in activity_grid]
 
-# loop through and add activity actograms to column
+# Plot a representative animal from each protocol on each axis
 for condition, axis in zip(conditions, activity_axes):
-
-    # select right number for each conditions
     data = activity_df.loc[condition]
     animal = anim_dict[condition]
     
-    # remove labels from subplot before adding in actogram
+    # Plot on a clean axis
     axis.set(yticks=[],
-             xticks=[],
-             title=condition)
-    
-    # plot actogram
+             xticks=[])
+    axis.text(-0.2, # panel text
+              1.1,
+              ac_panels[condition],
+              transform=axis.transAxes)
+    axis.text(-0.3, # condition text
+              0.5,
+              condition,
+              transform=axis.transAxes,
+              rotation=90)
     afig, acax = aplot._actogram_plot_from_df(data,
                                               animal,
                                               fig=fig,
                                               subplot=axis,
                                               timeaxis=True,
                                               **actogram_kwargs)
-    # tidy spacing labels and axis
+    
     acax.tick_params(axis="x", which='major', labelsize=timelabelsize)
-
-# create the right column for sleep
-sleep_grid = gs.GridSpec(nrows=len(conditions), ncols=1, figure=fig,
-                         left=0.6, hspace=hspace)
+    
+    
+# Plot sleep actograms on the right column
+sleep_grid = gs.GridSpec(nrows=len(conditions),
+                         ncols=1,
+                         figure=fig,
+                         left=0.55,
+                         right=0.85,
+                         hspace=hspace)
 sleep_axes = [plt.subplot(x) for x in sleep_grid]
 
-# same thing for sleep. Loop through conditions, select data, remove axis, plot
+# Plot same animal sleep actogram on right column
 for condition, axis_1 in zip(conditions, sleep_axes):
-    
-    # grab the data
     sleep_data = sleep_df.loc[condition]
     animal = anim_dict[condition]
     
-    # remove labels
+    # plot on clean axis
     axis_1.set(yticks=[],
-               xticks=[],
-               title=condition)
+               xticks=[])
+    axis_1.text(-0.2, # panel text
+                1.1,
+                sl_panels[condition],
+                transform=axis_1.transAxes)
+    sfig, sax  = aplot._actogram_plot_from_df(sleep_data,
+                                              animal,
+                                              fig=fig,
+                                              subplot=axis_1,
+                                              timeaxis=True,
+                                              ylim=sleep_ylim,
+                                              **actogram_kwargs)
     
-    # plot
-    sub_fig, last_ax  = aplot._actogram_plot_from_df(sleep_data,
-                                 animal,
-                                 fig=fig,
-                                 subplot=axis_1,
-                                 timeaxis=True,
-                                 ylim=sleep_ylim,
-                                 **actogram_kwargs)
+    # tidy timelabels
+    sax.tick_params(axis="x", which='major', labelsize=timelabelsize)
+    
+    
+# Add in further labels
+type_label_level = 1.1
+type_label_xval = 0.35
+fig.text(
+    type_label_xval,
+    type_label_level,
+    "Activity",
+    transform=activity_axes[0].transAxes
+)
+fig.text(
+    type_label_xval,
+    type_label_level,
+    "Sleep",
+    transform=sleep_axes[0].transAxes
+)
 
-    last_ax.tick_params(axis="x", which='major', labelsize=timelabelsize)
-    
+# save final version
 fig.set_size_inches(8.27, 11.69)
-
 plt.savefig(save_fig, dpi=1000)
 
 plt.close('all')
